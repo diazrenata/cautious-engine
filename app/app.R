ecb2b8b7836de028e2a698a3968bbbc1169da151
Renata <renatadiaz@arizona.edu>
2023-09-08 13:56:45 -0700

    add fxn to update runs

--- app/app.R
+++ app/app.R
@@ -3,8 +3,9 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 
-#runs_dat <- read.csv('runs.csv')
-runs_dat <- read.csv(here::here('app', 'runs.csv'))
+
+runs_dat <- read.csv('runs.csv')
+#runs_dat <- read.csv(here::here('app', 'runs.csv'))
 
 runs_dates <- runs_dat %>%
   mutate(Date = as.Date(Date, format = "%m/%d/%Y"),
@@ -12,81 +13,125 @@ runs_dates <- runs_dat %>%
 
 run_days <- unique(select(runs_dates, Date, Weekday))
 
-week_starts <- data.frame(Date = seq(min(run_days$Date) - 6, max(run_days$Date), by = "days")) %>%
-  mutate(Weekday = weekdays(Date)) %>% 
+week_starts <-
+  data.frame(Date = seq(min(run_days$Date) - 6, max(run_days$Date), by = "days")) %>%
+  mutate(Weekday = weekdays(Date)) %>%
   filter(Weekday == "Monday") %>%
   mutate(Week = row_number())
-week_ends <-  data.frame(Date = seq(min(run_days$Date), max(run_days$Date) + 6, by = "days")) %>%
-  mutate(Weekday = weekdays(Date)) %>% 
+week_ends <-
+  data.frame(Date = seq(min(run_days$Date), max(run_days$Date) + 6, by = "days")) %>%
+  mutate(Weekday = weekdays(Date)) %>%
   filter(Weekday == "Sunday") %>%
-  mutate(Week = row_number()) 
+  mutate(Week = row_number())
 week_numbers <- data.frame(
   Week = week_starts$Week,
   Week_start = week_starts$Date,
   Week_end  = week_ends$Date
 )
 
-run_week_numbers <- left_join(runs_dates, week_numbers, by = join_by(between(Date, Week_start, Week_end)))
+run_week_numbers <-
+  left_join(runs_dates, week_numbers, by = join_by(between(Date, Week_start, Week_end)))
 
 cumulative_week_miles <- run_week_numbers %>%
   group_by(Week) %>%
-  summarize(Miles = sum(Distance))
+  summarize(Miles = sum(Distance)) %>%
+  mutate(conservative = 11.1 * (1.1 ^ (Week - 2)))
 
 
 today <- as.Date(Sys.Date())
 
-days_since_today <- data.frame(
-  Date = seq(min(runs_dates$Date), today, by = "days")) %>%
+days_since_today <- data.frame(Date = seq(min(runs_dates$Date), today, by = "days")) %>%
   mutate(Week = ceiling(as.numeric(Date - today) / 7) + ceiling(nrow(.) / 7))
 
-running_week_starts <- days_since_today %>% 
+running_week_starts <- days_since_today %>%
   group_by(Week) %>%
   summarize(Week_start = min(Date),
             Week_end = max(Date)) %>%
   ungroup()
 
-running_week_numbers <- left_join(runs_dates, running_week_starts, by = join_by(between(Date, Week_start, Week_end)))
+running_week_numbers <-
+  left_join(runs_dates, running_week_starts, by = join_by(between(Date, Week_start, Week_end)))
 
 running_cumulative_week_miles <- running_week_numbers %>%
   group_by(Week) %>%
-  summarize(Miles = sum(Distance))
+  summarize(Miles = sum(Distance)) %>%
+  mutate(conservative = 11.1 * (1.1 ^ (Week - 2)))
 
 
 
 
-ui <- fluidPage(
-  titlePanel("Cumulative weekly distance"),
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(inputId = "week_type", label = "Week Start", choices = c("7 days ago", "Calendar week"), selected = "7 days ago")
-    ),
-    mainPanel(
-      plotOutput(outputId = "mileagePlot"),
-      textOutput(outputId = "mileageText")
-    )
-  )
-)
+ui <- fluidPage(titlePanel("Cumulative weekly distance"),
+                sidebarLayout(
+                  sidebarPanel(
+                    selectInput(
+                      inputId = "week_type",
+                      label = "Week Start",
+                      choices = c("7 days ago", "Calendar week"),
+                      selected = "7 days ago"
+                    )
+                  ),
+                  mainPanel(
+                    plotOutput(outputId = "mileagePlot"),
+                    textOutput(outputId = "mileageText")
+                  )
+                ))
 
 server <- function(input, output, session) {
-  
-  
-  makePlot <- reactive(
-    if(input$week_type == "Calendar week") {
-      ggplot(cumulative_week_miles, aes(Week, Miles)) + geom_line() + geom_point()
-    } else if(input$week_type == "7 days ago") {
-      ggplot(running_cumulative_week_miles, aes(Week, Miles)) + geom_line() + geom_point()
+  makePlot <- reactive(if (input$week_type == "Calendar week") {
+    ggplot(cumulative_week_miles,
+           aes(Week, Miles)) +
+      geom_line() +
+      geom_point() +
+      geom_line(aes(y = conservative), color = "red")
+  } else if (input$week_type == "7 days ago") {
+    ggplot(running_cumulative_week_miles, aes(Week, Miles)) +
+      geom_line() +
+      geom_point() +
       
-    }
-  )
+      geom_line(aes(y = conservative), color = "red")
+    
+  })
   
-  makeText <- reactive(
-    if(input$week_type == "Calendar week") {
-      paste0("For the week starting Monday ", max(week_starts$Date), " cumulative mileage is ", filter(cumulative_week_miles, Week == max(cumulative_week_miles$Week))$Miles)
-    } else if(input$week_type == "7 days ago") {
-      paste0("For the past 7 days counting from", today, " cumulative mileage is ", filter(running_cumulative_week_miles, Week == max(running_cumulative_week_miles$Week))$Miles)
-      
-    }
-  )
+  makeText <- reactive(if (input$week_type == "Calendar week") {
+    paste0(
+      "For the week starting Monday ",
+      max(week_starts$Date),
+      " cumulative mileage is ",
+      filter(
+        cumulative_week_miles,
+        Week == max(cumulative_week_miles$Week)
+      )$Miles,
+      ". The difference between this an a recommended max is ",
+      filter(
+        cumulative_week_miles,
+        Week == max(cumulative_week_miles$Week)
+      )$conservative - filter(
+        cumulative_week_miles,
+        Week == max(cumulative_week_miles$Week)
+      )$Miles,
+      "."
+    )
+  } else if (input$week_type == "7 days ago") {
+    paste0(
+      "For the past 7 days counting from",
+      today,
+      " cumulative mileage is ",
+      filter(
+        running_cumulative_week_miles,
+        Week == max(running_cumulative_week_miles$Week)
+      )$Miles,
+      ". The difference between this an a recommended max is ",
+      filter(
+        running_cumulative_week_miles,
+        Week == max(running_cumulative_week_miles$Week)
+      )$conservative - filter(
+        running_cumulative_week_miles,
+        Week == max(running_cumulative_week_miles$Week)
+      )$Miles,
+      "."
+    )
+    
+  })
   
   
   
@@ -102,4 +147,3 @@ server <- function(input, output, session) {
 
 
 shinyApp(ui, server)
-
