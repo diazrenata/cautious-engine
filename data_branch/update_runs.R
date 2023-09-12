f6c6fca241c03ded55af1d1eb3da8baf4aa79444
Renata <renatadiaz@arizona.edu>
2023-09-12 06:26:45 -0700

    update

--- data_branch/update_runs.R
+++ data_branch/update_runs.R
@@ -1,12 +1,12 @@
 update_runs <- function(mileage = 10, date = format(Sys.Date(), "%m/%d/%Y"), minutes = NA, seconds = NA) {
   
-  runs <- read.csv("runs.csv")
+  runs <- read.csv("data_branch/runs.csv")
   
   new_run <- data.frame(Date = date, Minutes = minutes, Seconds = seconds, Distance = mileage)
   
   runs <- rbind(runs, new_run)
   
-  write.csv(runs, "runs.csv", row.names = F)
+  write.csv(runs, "data_branch/runs.csv", row.names = F)
 }
 
 #update_runs()
